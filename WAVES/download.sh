19c9c71802872cdefc1922f364056ed66a4470a4
lucas <lucas.glasner@ceaza.cl>
2022-10-22 18:12:52 -0300

    Ahora se descarga el hindcast de olas

--- WAVES/download.sh
+++ WAVES/download.sh
@@ -5,61 +5,83 @@ printf '\nSTARTING DOWNLOAD OF MERCATOR FORECAST\n'
 echo "---------------------------------------------------------------------------------------------"
 cd /home/lucas/storage/FORECAST/MERCATOR/WAVES/
 idate=$(date +%F)
+ydate=$(date +%F -d '-1 day')
 fdate=$(date +%F -d '+9 day')
 echo "Creating config file for today..."
 echo "Configs:{ "
 printf '\n'
-cat motuconfig.ini | sed "s/%NAME%/${idate}/g" | sed "s/%TIME1%/${idate}\ 00:00/g" | sed "s/%TIME2%/${fdate}\ 23:59/g" > tmp.ini
-cat tmp.ini
+cat motuconfig.ini | sed "s/%NAME%/${idate}/g" | sed "s/%TIME1%/${idate}\ 00:00/g" | sed "s/%TIME2%/${fdate}\ 23:59/g" > forecast.ini
+cat motuconfig.ini | sed "s/%NAME%/HINDCAST\/${ydate}/g" | sed "s/%TIME1%/${ydate}\ 00:00/g" | sed "s/%TIME2%/${ydate}\ 23:59/g" > hindcast.ini
+cat forecast.ini
 printf '\n\n}\n'
 if [ -f "${idate}.nc" ]; then
-    echo "Data for today already exists!"
-    echo "Checking file integrity..."
+    echo "Forecast for today already exists!"
+    echo "Checking forecast file integrity..."
     statement=$(ncdump -h ${idate}.nc | head | grep -oP '(?<=time =).*?(?=;)')
     if [ $statement = 80 ]; then
         echo 'All good'
     else
-        echo 'File isnt complete, downloading again...'
-        motuclient --config-file tmp.ini
+        echo 'Forecast file isnt complete, downloading again...'
+        motuclient --config-file forecast.ini
     fi
 else
-    echo "Data for today doesnt exists"
+    echo "Forecast data for today doesnt exists"
     echo "Downloading forecast..."
-    motuclient --config-file tmp.ini
+    motuclient --config-file forecast.ini
 fi
-rm tmp.ini
+printf '\n'
+if [ -f "HINDCAST/${ydate}.nc" ]; then
+	echo "Hindcast for today already exists!"
+else
+	echo "Hindcast data for today doesnt exists!"
+	echo "Downloading hindcast..."
+	motuclient --config-file hindcast.ini
+fi
+
+rm hindcast.ini
+rm forecast.ini
 printf "Done\n"
 printf "\n\n"
 echo "---------------------------------------------------------------------------------------------"
 echo "Checking data for the last 10 days..."
 for i in {1..10}; do
     printf '\n'
-    echo "-------------------------------------------------------------------------------------------"
+    echo "---------------------------------------------------------------------------------------------"
     printf "\nChecking forecast for $i days ago...\n"
     idate=$(date +%F -d "-$i day")
+    ydate=$(date +%F -d "-$(( $i+1 )) day")
     n=$(( 9-$i ))
     fdate=$(date +%F -d "+$n day")
     echo "Creating config file for $i days ago..."
     echo "Configs:{ "
     printf '\n'
-    cat motuconfig.ini | sed "s/%NAME%/${idate}/g" | sed "s/%TIME1%/${idate}\ 00:00/g" | sed "s/%TIME2%/${fdate}\ 23:59/g" > tmp.ini
-    cat tmp.ini
+    cat motuconfig.ini | sed "s/%NAME%/${idate}/g" | sed "s/%TIME1%/${idate}\ 00:00/g" | sed "s/%TIME2%/${fdate}\ 23:59/g" > forecast.ini
+    cat motuconfig.ini | sed "s/%NAME%/HINDCAST\/${ydate}/g" | sed "s/%TIME1%/${ydate}\ 00:00/g" | sed "s/%TIME2%/${ydate}\ 23:59/g" > hindcast.ini
+    cat forecast.ini
     printf '\n\n}\n'
     if [ -f "${idate}.nc" ]; then
-        echo "Data for $i days ago already exists!"
+        echo "Forecast data for $i days ago already exists!"
         echo "Checking file integrity..."
         statement=$(ncdump -h ${idate}.nc | head | grep -oP '(?<=time =).*?(?=;)')
         if [ $statement = 80 ]; then
             echo 'All good'
         else
-            echo 'File isnt complete, downloading again...'
-            motuclient --config-file tmp.ini
+            echo 'Forecast file isnt complete, downloading again...'
+            motuclient --config-file forecast.ini
         fi
     else
-        echo "Data for $i days ago doesnt exists"
+        echo "Forecast data for $i days ago doesnt exists"
         echo "Downloading forecast..."
-        motuclient --config-file tmp.ini
+        motuclient --config-file forecast.ini
     fi
-    rm tmp.ini
+    if [ -f "HINDCAST/${ydate}.nc" ]; then
+	    echo "Hindcast data for $(( $i+1 )) days ago already exists!"
+    else
+	    echo "Hindcast data for $(( $i+1 )) days ago doesnt exists"
+	    echo "Downloading hindcast..."
+	    motuclient --config-file hindcast.ini
+    fi
+    rm forecast.ini
+    rm hindcast.ini
     printf "Done\n"
 done
